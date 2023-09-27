27208ac99059cf70a32045af12ffe22c1ea9c28c
narahma2 <rahmann@purdue.edu>
2023-09-27 12:22:06 -0500

    Adding new functions

--- src/pyxeol/__init__.py
+++ src/pyxeol/__init__.py
@@ -2,8 +2,12 @@ from importlib.metadata import version
 
 from pyxeol import laue
 from pyxeol import misc
+from pyxeol import readMDA
+from pyxeol import scanInfo
 from pyxeol import specfun
+from pyxeol import xbic
 from pyxeol import xeol
+from pyxeol import xrf
 
 
 try:
